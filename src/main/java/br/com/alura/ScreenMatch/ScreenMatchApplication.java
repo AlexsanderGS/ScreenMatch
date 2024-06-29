c38d02cca6c1792e5644bc5175039e6a53788999
AlexsanderGS <alexsanderga@gmail.com>
2024-06-29 15:36:29 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.ScreenMatch;
+
+import br.com.alura.ScreenMatch.Service.ConsumoAPI;
+import br.com.alura.ScreenMatch.Service.ConverteDados;
+import br.com.alura.ScreenMatch.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://omdbapi.com/?t=gilmore+girls&apikey=df04059f");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}

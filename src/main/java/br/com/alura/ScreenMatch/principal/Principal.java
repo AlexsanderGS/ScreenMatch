985acd352dc0d109376d19a7ad6d5d401ae0e9ed
AlexsanderGS <alexsanderga@gmail.com>
2024-07-13 21:31:58 -0300

    Complementando a classe Serie, para que tenhamos informações mais acertivas sobre as séries escolhidas pelo usuário.

--- src/main/java/br/com/alura/ScreenMatch/principal/Principal.java
+++ src/main/java/br/com/alura/ScreenMatch/principal/Principal.java
@@ -4,10 +4,13 @@ import br.com.alura.ScreenMatch.Service.ConsumoAPI;
 import br.com.alura.ScreenMatch.Service.ConverteDados;
 import br.com.alura.ScreenMatch.model.DadosSerie;
 import br.com.alura.ScreenMatch.model.DadosTemporadas;
+import br.com.alura.ScreenMatch.model.Serie;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -84,6 +87,13 @@ public class Principal {
     }
 
     private void listarSeriesBuscadas(){
-        dadosSeries.forEach(System.out::println);
+
+        List<Serie> series = new ArrayList<>();
+        series = dadosSeries.stream()
+                        .map(d -> new Serie(d))
+                                .collect(Collectors.toList());
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
     }
 }
